ea400545e4f01cedc82ada7dd74d70ffbc79645d
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-07-27 16:36:21 -0600

    adding basic React files

---
+++ client/src/components/users.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/*const users = () => {
+  return (
+    <users>
+      <p>Text</p>
+    </users>
+  );
+};
+*/
+
+export default users;
---
+++ client/src/components/usersList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/*const usersList = () => {
+  return (
+    <usersList>
+      <p>Text</p>
+    </usersList>
+  );
+};
+*/
+
+export default usersList;
