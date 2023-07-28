cd4b274bff64035ac05f27020aef346cb7a50629
Jordan Olguin <jordanolguin@iMac.local>
2023-07-27 21:17:45 -0600

    pair programmed resolvers and typeDevs with Derek

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const User = require("./User");
+const Chat = require("./Chat");
+const Message = require("./Message");
 
-module.exports = { User };
+module.exports = { User, Chat, Message };
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,55 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
-const { signToken } = require("../utils/auth");
+const { User, Chat, Message } = require("../models");
+// const { signToken } = require("../utils/auth");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    users: async () => {
+      return await User.find().populate("friends");
+    },
+    user: async (parent, { username }, context) => {
+      return await User.findOne({ username }).populate("friends");
+    },
+    userChats: async (parent, { username }, context) => {
+      return await Chat.find({
+        $or: [{ user1Id: User }, { user2Id: User }],
+      }).populate({
+        path: "messages",
+        populate: { path: "sender" },
+      });
+    },
+    chat: async (parent, { chatId }, context) => {
+      return await Chat.findById(chatId).populate({
+        path: "messages",
+        populate: { path: "sender" },
+      });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
+};
 
 module.exports = resolvers;
