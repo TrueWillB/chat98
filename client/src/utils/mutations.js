4f9d5a23c589b5b89df88761aa252b7ce3539cc3
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-07-29 20:45:59 -0600

    add login and signup modal

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,87 +25,87 @@ export const LOGIN_USER = gql`
   }
 `;
 
-export const REMOVE_FRIEND = gql`
-    mutation removeFriend(username:String!, friendId:ID!){
-    removeFriend(username:$username, friendId:$friendId){
-        _id
-        username
-        friends{
-            _id
-            username
-        }
-    }
-}
-`;
+// export const REMOVE_FRIEND = gql`
+//     mutation removeFriend(username:String!, friendId:ID!){
+//     removeFriend(username:$username, friendId:$friendId){
+//         _id
+//         username
+//         friends{
+//             _id
+//             username
+//         }
+//     }
+// }
+// `;
 
-export const SEND_FRIEND_REQUEST = gql`
-   mutation sendFriendRequest(senderId:ID!, receiverId:ID!){
-    sendFriendRequest(senderId:$senderId, receiverId:$receiverId){
-        _id
-        username
-        pendingFriends{
-            _id
-            username
-        }
-    }
-}
-`;
+// export const SEND_FRIEND_REQUEST = gql`
+//    mutation sendFriendRequest(senderId:ID!, receiverId:ID!){
+//     sendFriendRequest(senderId:$senderId, receiverId:$receiverId){
+//         _id
+//         username
+//         pendingFriends{
+//             _id
+//             username
+//         }
+//     }
+// }
+// `;
 
-export const APPROVE_FRIEND_REQUEST = gql`
-    mutation approveFriendRequest(senderId:ID!, receiverId:ID!){
-        approveFriendRequest(senderId:$senderId, receiverId:$receiverId){
-        _id
-        username
-        friends{
-            _id
-            username
-        }
-    }
-}
-`;
+// export const APPROVE_FRIEND_REQUEST = gql`
+//     mutation approveFriendRequest(senderId:ID!, receiverId:ID!){
+//         approveFriendRequest(senderId:$senderId, receiverId:$receiverId){
+//         _id
+//         username
+//         friends{
+//             _id
+//             username
+//         }
+//     }
+// }
+// `;
 
-export const REJECT_FRIEND_REQUEST = gql`
-    mutation rejectFriendRequest(senderId:ID!, receiverId:ID!){
-        rejectFriendRequest(senderId:$senderId, receiverId:$receiverId){
-        _id
-        username
-        pendingFriends{
-            _id
-            username
-        }
-    }
-}
-`;
+// export const REJECT_FRIEND_REQUEST = gql`
+//     mutation rejectFriendRequest(senderId:ID!, receiverId:ID!){
+//         rejectFriendRequest(senderId:$senderId, receiverId:$receiverId){
+//         _id
+//         username
+//         pendingFriends{
+//             _id
+//             username
+//         }
+//     }
+// }
+// `;
 
-export const START_CHAT = gql`
-    mutation startChat($user1Id: ID!, user2Id: ID!) {
-        startChat(user1Id: $user1Id, user2Id: $user2Id) {
-            _id
-            user1Id
-            user2Id
-            messages {
-                _id
-                senderId {
-                    _id
-                    username
-                }
-                content
-                readStatus
-            }
-        }
-    }
-`;
+// export const START_CHAT = gql`
+//     mutation startChat($user1Id: ID!, user2Id: ID!) {
+//         startChat(user1Id: $user1Id, user2Id: $user2Id) {
+//             _id
+//             user1Id
+//             user2Id
+//             messages {
+//                 _id
+//                 senderId {
+//                     _id
+//                     username
+//                 }
+//                 content
+//                 readStatus
+//             }
+//         }
+//     }
+// `;
 
-export const SEND_MESSAGE = gql`
-  mutation sendMessage($chatId: ID!, $senderId: ID!, $content: String!) {
-    sendMessage(chatId: $chatId, senderId: $senderId, content: $content) {
-      _id
-      senderId {
-        _id
-        username
-      }
-      content
-      readStatus
-    }
-  }
-`;
+// export const SEND_MESSAGE = gql`
+//   mutation sendMessage($chatId: ID!, $senderId: ID!, $content: String!) {
+//     sendMessage(chatId: $chatId, senderId: $senderId, content: $content) {
+//       _id
+//       senderId {
+//         _id
+//         username
+//       }
+//       content
+//       readStatus
+//     }
+//   }
+// `;
