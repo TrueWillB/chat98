319ada5d0515d87f92951d89f3b5ac6f81f300e6
William Brown <willis291@gmail.com>
2023-07-31 23:09:39 -0600

    sidebar now renders all users. Buttons are created next to users, sendFriendRequest button working

--- client/src/components/friendsList.js
+++ client/src/components/friendsList.js
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
-import { UserPlus, UserMinus } from "feather-icons-react";
+import { UserPlus, UserMinus, User } from "feather-icons-react";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USERS, QUERY_USER } from "../utils/queries";
-import { REMOVE_FRIEND } from "../utils/mutations";
-import Autocomplete from "@mui/material/Autocomplete";
+import { REMOVE_FRIEND, SEND_FRIEND_REQUEST } from "../utils/mutations";
+// import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import auth from "../utils/auth";
 
 const FriendsList = (props) => {
   const profile = auth.getProfile();
-  const { loading, error, data, refetch } = useQuery(QUERY_USER, {
+  const {
+    loading,
+    error,
+    data: currentUserData,
+    refetch,
+  } = useQuery(QUERY_USER, {
     variables: { username: profile.data.username },
   });
+
+  //this is the userId of the currently logged in user. To be used for friend add/remove
+  const currentUser = currentUserData?.user;
+  const currentUserId = currentUserData?.user?._id;
 
   const {
     loading: loadingAllUsers,
@@ -19,6 +28,25 @@ const FriendsList = (props) => {
     data: userListData,
   } = useQuery(QUERY_USERS);
   const [userSearch, setUserSearch] = useState("");
+
+  const [sendFriendRequest] = useMutation(SEND_FRIEND_REQUEST);
+
+  const handleAddFriend = async (friendIdToAdd) => {
+    console.log("clicked friend add button");
+    console.log("friendIdToAdd: ", friendIdToAdd);
+    console.log("currentUserId: ", currentUserId);
+    try {
+      await sendFriendRequest({
+        variables: { senderId: currentUserId, receiverId: friendIdToAdd },
+      });
+      console.log("Friend request sent!");
+      refetch();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log("Here's list of all users");
+  console.log(userListData?.users);
 
   // const [removeFriend] = useMutation(REMOVE_FRIEND);
   //
@@ -34,39 +62,51 @@ const FriendsList = (props) => {
   //   }
   // };
 
-  const listForAutocomplete = userListData?.users?.map((user) => user.username);
+  //listForAutocomplete is an array of all the user objects
+  const listForAutocomplete = userListData?.users || [];
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  if (error) return <p>Error :</p>;
 
   return (
     <React.Fragment>
-      <div class="searchbarContainer">
-        <Autocomplete
-          className="searchInput"
-          type="text"
-          options={listForAutocomplete}
-          sx={{ width: 300 }}
-          placeholder="Find friends"
-          onChange={(e) => setUserSearch(e.target.value)}
-          renderInput={(params) => (
-            <TextField {...params} label="Search Usernames" />
-          )}
-        />
+      <div className="searchbarContainer">
+        <ul style={{ listStyleType: "none" }}>
+          <TextField
+            label="Search Usernames"
+            onChange={(e) => setUserSearch(e.target.value)}
+          />
+          {listForAutocomplete
+            .filter((user) =>
+              user.username.toLowerCase().includes(userSearch.toLowerCase())
+            )
+            .map((user, index) => (
+              <li className="user-in-user-list" key={user.username}>
+                {user.username}
+                {/* This is a conditional render to prevent button from loading if they are already a pending friend */}
+                {user.pendingFriends
+                  .map((user) => user._id)
+                  .includes(currentUserId) ? (
+                  <User stroke="Gray" size={25} />
+                ) : user.friends
+                    .map((friend) => friend._id)
+                    .includes(currentUserId) ? (
+                  <UserMinus
+                    onClick={() => console.log("clicked remove friend button")}
+                    stroke="Red"
+                    size={25}
+                  />
+                ) : (
+                  <UserPlus
+                    onClick={() => handleAddFriend(user._id)}
+                    stroke="Blue"
+                    size={25}
+                  />
+                )}
+              </li>
+            ))}
+        </ul>
       </div>
-      {/* <div id="usersContainer">
-        {data?.user?.friends?.map((friend) => (
-          <div key={friend._id} className="userList">
-            <span className="username">{friend.username}</span>
-            <button
-              className="friendButton"
-              onClick={() => handleRemoveFriend(friend._id)}
-            >
-              <UserMinus stroke="red" size={16} />
-            </button>
-          </div>
-        ))}
-      </div> */}
     </React.Fragment>
   );
 };
