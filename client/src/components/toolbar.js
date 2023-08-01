a128228fc25cf63a8f5112a2e0eb4cb585db3bdb
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-08-01 10:33:39 -0600

    added 98 theme

--- client/src/components/toolbar.js
+++ client/src/components/toolbar.js
@@ -3,23 +3,22 @@ import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Login from "./modals/login";
+import Logo from "./Chat98.png";
+
 // temporary: friend request button is a placeholder for alerting user to new requests
 
 const Toolbar = () => {
   return (
     <div id="toolbar">
-      <div id="toolbarButtons">
-        {" "}
-        <Stack spacing={2} direction="row">
-          <Button variant="contained">Theme</Button>
-          <Login />
-          <Button variant="contained" component={Link} to="/friend-requests">
-            Requests
-          </Button>
-          <Button variant="contained" component={Link} to="/search">
-            Search
-          </Button>
-        </Stack>
+      <div id="toolbarButtonsLeft">
+        <img src={Logo} alt="Chat98 Logo" id="logo" />
+        <Login class="homeButtons" />
+        <Button class="homeButtons">Time Travel</Button>
+      </div>
+      <div id="toolbarButtonsRight">
+        <Button class="homeButtons" component={Link} to="/friend-requests">
+          Requests
+        </Button>
       </div>
     </div>
   );
