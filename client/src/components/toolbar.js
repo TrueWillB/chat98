5e62fd1a49936895b4fd157a183dfcbf3595bcec
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-08-02 10:48:53 -0600

    hide home and request buttons when not logged in. Send user to home screen when login is clicked

--- client/src/components/toolbar.js
+++ client/src/components/toolbar.js
@@ -4,7 +4,7 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import Login from "./modals/login";
 import Logo from "./Chat98.png";
-
+import Auth from "../utils/auth";
 // temporary: friend request button is a placeholder for alerting user to new requests
 
 const Toolbar = () => {
@@ -15,14 +15,22 @@ const Toolbar = () => {
         <Login class="homeButtons" />
         <Button class="homeButtons">Time Travel</Button>
       </div>
-      <div id="toolbarButtonsRight">
-        <Button class="homeButtons" id="homeButton" component={Link} to="/home">
-          Home
-        </Button>
-        <Button class="homeButtons" component={Link} to="/friend-requests">
-          Requests
-        </Button>
-      </div>
+      {!Auth.loggedIn(true) ? (
+        <div></div>
+      ) : (
+        <div>
+          <Button className="homeButtons" component={Link} to="/home">
+            Home
+          </Button>
+          <Button
+            className="homeButtons"
+            component={Link}
+            to="/friend-requests"
+          >
+            Requests
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
